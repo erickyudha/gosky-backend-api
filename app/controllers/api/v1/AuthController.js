cbf293063c1f8089f3558a2ab452f41d73bd8310
Erick Yudha <erickyudha18@gmail.com>
2022-11-25 20:39:19 +0700

    jsdoc test

--- app/controllers/api/v1/AuthController.js
+++ app/controllers/api/v1/AuthController.js
@@ -1,24 +1,80 @@
+const {
+  UnauthorizedError,
+} = require('../../../errors');
+
+/**
+   * Base authorize middleware model
+   * @param {*} req - Express req instance
+   * @param {*} res - Express res instance
+   * @param {Function} next - Next function
+   * @param {Array} role - Allowed roles to authorize
+   * @return {Function} Role spesific authorize function
+   */
+baseAuthorize = async (req, res, next, role) => {
+  try {
+    const bearerToken = req.headers.authorization;
+    const token = bearerToken.split('Bearer ')[1];
+
+    // TODO: VERIFY TOKEN AND CHECK USER
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json(new UnauthorizedError().json());
+  }
+};
+
+/** Class representing an auth endpoint controller.*/
 class AuthController {
-  constructor({
-    userModel,
-    bcrypt,
-    jwt,
-  }) {
+  /**
+   * Create an authentication controller
+   * @param {SequelizeModel} userModel - Sequelize model for Users table
+   * @param {Module} bcrypt - Bcrypt module
+   * @param {Module} jwt - JWT module
+   */
+  constructor(userModel, bcrypt, jwt) {
     this.userModel = userModel;
     this.bcrypt = bcrypt;
     this.jwt = jwt;
   }
 
-  authorize = (role) => {
-    return (req, res, next) > {
-
-    };
+  /**
+   * Authorize middleware for user and admin level role
+   * @param {*} req - Express req instance
+   * @param {*} res - Express res instance
+   * @param {Function} next - Next function
+   */
+  authorizeUser = (req, res, next) => {
+    baseAuthorize(req, res, next, ['USER', 'ADMIN']);
   };
 
+
+  /**
+   * Authorize middleware for admin level role
+   * @param {*} req - Express req instance
+   * @param {*} res - Express res instance
+   * @param {Function} next - Next function
+   */
+  authorizeAdmin = (req, res, next) => {
+    baseAuthorize(req, res, next, ['ADMIN']);
+  };
+
+
+  /**
+   * Handle register endpoint
+   * @param {*} req - Express req instance
+   * @param {*} res - Express res instance
+   */
   handleRegister = async (req, res) => {
 
   };
 
+
+  /**
+   * Handle login endpoint
+   * @param {*} req - Express req instance
+   * @param {*} res - Express res instance
+   */
   handleLogin = async (req, res) => {
 
   };
