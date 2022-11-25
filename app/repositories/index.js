8aebac13c98fa6d3dd1a905d52747e9fe3f5a4a0
Erick Yudha <erickyudha18@gmail.com>
2022-11-25 20:35:18 +0700

    setup repository folder

---
+++ app/repositories/index.js
@@ -0,0 +1,9 @@
+const ticketRepository = require('./ticketRepository');
+const transactionRepository = require('./transactionRepository');
+const userRepository = require('./userRepository');
+
+module.exports = {
+  ticketRepository,
+  transactionRepository,
+  userRepository,
+};
