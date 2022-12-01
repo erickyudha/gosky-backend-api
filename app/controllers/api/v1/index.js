464379ff85c0acbbb1baf38bfc0fc441f9d38567
Erick Yudha <erickyudha18@gmail.com>
2022-12-01 18:56:42 +0700

    add index

--- app/controllers/api/v1/index.js
+++ app/controllers/api/v1/index.js
@@ -1,7 +1,9 @@
 const AuthController = require('./AuthController');
 const TicketController = require('./TicketController');
+const UserController = require('./UserController');
 
 module.exports = {
   AuthController,
   TicketController,
+  UserController,
 };
--- app/services/index.js
+++ app/services/index.js
@@ -1,9 +1,13 @@
 const userService = require('./userService');
 const ticketService = require('./ticketService');
 const transactionService = require('./transactionService');
+const authService = require('./authService');
+const emailService = require('./emailService');
 
 module.exports = {
   userService,
   ticketService,
   transactionService,
+  authService,
+  emailService,
 };
