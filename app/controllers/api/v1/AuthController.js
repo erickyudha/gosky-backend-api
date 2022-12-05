b0da1908c955cba38938ef3ef576a51a8313f40d
riowidywasa <riowidywasa@gmail.com>
2022-12-05 20:23:21 +0700

    auth controller and user controller done
    Signed-off-by: riowidywasa <riowidywasa@gmail.com>

--- app/controllers/api/v1/AuthController.js
+++ app/controllers/api/v1/AuthController.js
@@ -1,19 +1,30 @@
+const services = require('../../../services');
+const {
+  GeneralError,
+  MissingFieldError,
+} = require('../../../errors');
 const {
   UnauthorizedError,
 } = require('../../../errors');
 
 baseAuthorize = async (req, res, next, role) => {
   try {
-    // const bearerToken = req.headers.authorization;
-    // const token = bearerToken.split('Bearer ')[1];
-
+    const bearerToken = req.headers.authorization;
+    const token = bearerToken.split('Bearer ')[1];
     // TODO: VERIFY TOKEN AND CHECK USER ROLE
-
+    const payload = services.authService.decodeUserToken(token);
     // if success pass user data to req.user and go next()
+    const user = await services.userService.getByEmail(payload);
+    if (!role.includes(user.role)) {
+      res.status(401).json(new UnauthorizedError().json());
+      return;
+    }
     req.user = user;
     next();
   } catch (err) {
-    res.status(401).json(new UnauthorizedError().json());
+    const error = new GeneralError(err);
+    res.status(500).json(error.json());
+    return;
   }
 };
 class AuthController {
@@ -37,41 +48,166 @@ class AuthController {
     // TODO:
     // Finish handleGetOtp First
     // email is in otpToken
+    try {
+      this.authService.verifyOtpToken(req.body.otp, req.body.otpToken);
+      if (
+        !req.body.name ||
+        !req.body.encryptedPassword ||
+        !req.body.otp ||
+        !req.body.otpToken
+      ) {
+        const error = new MissingFieldError();
+        res.status(400).json(error.json());
+      } else {
+        const verify = this.authService.verifyOtpToken(
+            req.body.otp, req.body.otpToken,
+        );
+        if (!verify) {
+          const error = new GeneralError();
+          res.status(500).json(error.json());
+          return;
+        } else {
+          const encryptPassword = await this.authService.encryptPassword(
+              req.body.encryptedPassword);
+          await this.userService.create({
+            email: req.query.email,
+            ...req.body,
+            encryptedPassword: encryptPassword,
+            role: 'USER',
+          });
+          const accessToken = this.authService.createTokenFromUser(verify);
+          res.status(201).json({
+            status: 'success',
+            message: 'register success',
+            data:
+              {accessToken: accessToken},
+          });
+        }
+      };
+    } catch (err) {
+      const error = new GeneralError(err);
+      res.status(500).json(error.json());
+      return;
+    }
   };
 
   handleLogin = async (req, res) => {
-    // TODO:
-    // Just regular login handler
+    try {
+      if (!req.body.email || !req.body.encryptedPassword) {
+        const error = new MissingFieldError();
+        res.status(400).json(error.json());
+      } else {
+        const email = req.body.email;
+        const password = req.body.encryptedPassword;
+        const user = await this.userService.getByEmail(email);
+
+        if (!user) {
+          res.status(401).json({
+            status: 'failed',
+            message: 'Email is not registered',
+          });
+          return;
+        }
+        console.log(user.encryptedPassword);
+        const isPasswordCorrect = this.authService.verifyPassword(
+            password, user.encryptedPassword);
+
+        if (!isPasswordCorrect) {
+          res.status(401).json({
+            status: 'failed',
+            message: 'Wrong password',
+          });
+          return;
+        }
+
+        const accessToken = this.authService.createTokenFromUser(email);
+        res.status(200).json({
+          status: 'success',
+          message: 'login success',
+          data:
+              {accessToken: accessToken},
+        });
+      };
+    } catch (err) {
+      const error = new GeneralError(err);
+      res.status(500).json(error.json());
+      return;
+    }
   };
 
   handleGetSelf = async (req, res) => {
     // TODO:
     // Just return data from req.user
+    res.status(200).json({
+      status: 'success',
+      message: 'get user data success',
+      data: {
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email,
+        encryptedPassword: req.user.encryptedPassword,
+        role: req.user.role,
+        phone: req.user.phone,
+        address: req.user.address,
+        imageId: req.user.imageId,
+        imageUrl: req.user.imageUrl,
+        createdAt: req.user.createdAt,
+        updatedAt: req.user.updatedAt,
+        deletedAt: req.user.deletedAt,
+      },
+    });
   };
 
   handleGetOtp = async (req, res) => {
-    // TODO:
-    // VERIFY email format IMPORTANT!
-    // Generate otp token with authService
-
-    // Set .env for email service
-    // Send otp email with emailService
-    // EXAMPLE:
-    // this.emailService.sendOtpEmail('email@mail.com', 123456,
-    //     (err, info) => {
-    //       if (err) {
-    //         throw new Error('failed to send email');
-    //       } else {
-    //         res.status(200).json({
-    //           'status': 'success',
-    //           'message': 'otp request success',
-    //           'data': {
-    //             'otpToken': CHANGE_THIS,
-    //           },
-    //         });
-    //       }
-    //     },
-    // );
+    try {// TODO:
+      // VERIFY email format IMPORTANT!
+      // Generate otp token with authService
+      const email = req.query.email;
+      const gmailAlreadyRegistered = await this.userService.getByEmail(email);
+      if (gmailAlreadyRegistered) {
+        res.status(409).json({
+          status: 'failed',
+          message: 'email already registered',
+        });
+        return;
+      }
+      if (!email) {
+        const error = new MissingFieldError();
+        res.status(400).json(error.json());
+        return;
+      }
+      const valid = email.indexOf('@gmail.com');
+      if (valid >= 0) {
+        const createOtpTokenEmail = this.authService.createOtpToken(email);
+        const otp = this.authService.decodeUserToken(createOtpTokenEmail);
+        // Set .env for email service
+        // Send otp email with emailService
+        // EXAMPLE:
+        this.emailService.sendOtpEmail(email, otp.otp,
+            (err, info) => {
+              if (err) {
+                throw err;
+              } else {
+                res.status(200).json({
+                  'status': 'success',
+                  'message': 'otp request success',
+                  'data': {
+                    'otpToken': createOtpTokenEmail,
+                  },
+                });
+              }
+            },
+        );
+      } else {
+        res.status(500).json({
+          message: 'wrong Gmail format',
+        });
+      }
+    } catch (err) {
+      const error = new GeneralError();
+      res.status(500).json(error.json());
+      return;
+    }
   };
 };
 
--- app/controllers/api/v1/UserController.js
+++ app/controllers/api/v1/UserController.js
@@ -1,30 +1,147 @@
+
+const {
+  GeneralError,
+  IdNotFoundError,
+  MissingFieldError,
+} = require('../../../errors');
 class UserController {
   constructor(userService, authService) {
     this.userService = userService;
     this.authService = authService;
   }
 
-  handleGetSimpleUser = (req, res) => {
+  handleGetSimpleUser = async (req, res) => {
     // TODO:
     // GET SIMPLE USER DATA BY USER ID
     // id get from req.params
     // Check response example from swagger docs
+    try {
+      const user = await this.userService.simpleGet(req.params.id);
+      console.log(user);
+      if (!user) {
+        const err = new IdNotFoundError();
+        res.status(404).json(err.json());
+        return;
+      }
+      res.status(200).json({
+        status: 'success',
+        message: 'get user data success',
+        data: user,
+      });
+    } catch (err) {
+      const error = new GeneralError(err);
+      res.status(500).json(error.json());
+    }
   };
 
-  handleUpdateUser = (req, res) => {
+  handleUpdateUser = async (req, res) => {
     // TODO:
     // Finish authorization handler first
     // UPDATE USER DATA BY THAT USER THEMSELF
     // USER DATA GET FROM req.user
     // Check allowed field to update from swagger docs
+    try {
+      const user = req.user;
+      if (
+        !req.body.name ||
+        !req.body.phone ||
+        !req.body.address ||
+        !req.body.imageId ||
+        !req.body.imageUrl
+      ) {
+        const error = new MissingFieldError();
+        res.status(400).json(error.json());
+        return;
+      }
+      await this.userService.update(user.id, {
+        ...req.body,
+      });
+      const accessToken = await this.authService.createTokenFromUser(
+          user.email);
+      res.status(200).json({
+        status: 'success',
+        message: 'update user data success',
+        data: {
+          accessToken: accessToken,
+          userData: {
+            id: req.user.id,
+            name: req.body.name,
+            email: req.user.email,
+            encryptedPassword: req.user.encryptedPassword,
+            role: req.user.role,
+            phone: req.body.phone,
+            address: req.body.address,
+            imageId: req.body.imageId,
+            imageUrl: req.body.imageUrl,
+            createdAt: req.user.createdAt,
+            updatedAt: req.user.updatedAt,
+            deletedAt: req.user.deletedAt,
+          },
+        },
+      });
+    } catch (err) {
+      const error = new GeneralError(err);
+      res.status(500).json(error.json());
+    }
   };
 
-  handleUpdateUserEmail = (req, res) => {
+  handleUpdateUserEmail = async (req, res) => {
     // TODO:
     // Finish authorization handler first
     // UPDATE USER EMAIL BY THAT USER THEMSELF
     // USER DATA GET FROM req.user
     // Email get from decoded otpToken
+    try {
+      const user = req.user;
+      if (
+        !req.body.otp ||
+        !req.body.otpToken
+      ) {
+        const error = new MissingFieldError();
+        res.status(400).json(error.json());
+        return;
+      }
+      const verify = this.authService.verifyOtpToken(
+          req.body.otp, req.body.otpToken,
+      );
+      if (!verify) {
+        res.status(422).json({
+          status: 'failed',
+          message: 'wrong otp or invalid otpToken',
+        });
+        return;
+      }
+      const otp = this.authService.decodeUserToken(req.body.otpToken);
+      await this.userService.update(user.id, {
+        email: otp.email,
+      });
+      const accessToken = await this.authService.createTokenFromUser(
+          otp.email);
+      res.status(200).json({
+        status: 'success',
+        message: 'update user email success',
+        data: {
+          accessToken: accessToken,
+          userData: {
+            id: req.user.id,
+            name: req.user.name,
+            email: otp.email,
+            encryptedPassword: req.user.encryptedPassword,
+            role: req.user.role,
+            phone: req.user.phone,
+            address: req.user.address,
+            imageId: req.user.imageId,
+            imageUrl: req.user.imageUrl,
+            createdAt: req.user.createdAt,
+            updatedAt: req.user.updatedAt,
+            deletedAt: req.user.deletedAt,
+          },
+        },
+      });
+    } catch (err) {
+      const error = new GeneralError(err);
+      res.status(500).json(error.json());
+    }
   };
 };
 
