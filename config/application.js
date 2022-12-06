4a6c91b62202b042a038adac72565e5ecd3736aa
Erick Yudha <erickyudha18@gmail.com>
2022-12-06 12:42:41 +0700

    minor big fix

--- config/application.js
+++ config/application.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 module.exports = {
   MORGAN_FORMAT:
     ':method :url :status :res[content-length] - :response-time ms',
   JWT_SIGNATURE_KEY: process.env.JWT_SIGNATURE_KEY || 'Rahasia',
   EMAIL_SIGNATURE_KEY: process.env.JWT_SIGNATURE_KEY || 'Rahasia2',
-  SALT: process.env.SALT || 10,
+  SALT: parseInt(process.env.SALT) || 10,
 };
