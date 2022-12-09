ac2d8852947a7023bb5a7cc2ea1231f9f68fa5ff
Erick Yudha <erickyudha18@gmail.com>
2022-12-09 11:56:59 +0700

    remove test router

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,11 +1,9 @@
 const docsRouter = require('./docsRouter');
 const apiRouter = require('./apiRouter');
-const testRouter = require('./testRouter');
 
 function apply(app) {
   app.use('', docsRouter);
   app.use('/api', apiRouter);
-  app.use('/test', testRouter);
 
   const generalErrorHandler = (err, req, res, next) => {
     res.status(500).json({
