96888a04051d6a3308b4d17a2cfb56a424f49bfc
Erick Yudha <erickyudha18@gmail.com>
2022-12-20 18:09:45 +0700

    otp and transaction email done

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,9 +1,12 @@
 const docsRouter = require('./docsRouter');
 const apiRouter = require('./apiRouter');
+const path = require('path');
+const express = require('express');
 
 function apply(app) {
   app.use('', docsRouter);
   app.use('/api', apiRouter);
+  app.use('/static', express.static(path.join(__dirname, '../../public')));
 
   const generalErrorHandler = (err, req, res, next) => {
     res.status(500).json({
