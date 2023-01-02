30c9e65fb61e34fe5aaf69b9830208a21835d2a2
Erick Yudha <erickyudha18@gmail.com>
2023-01-02 21:11:34 +0700

    modify db user seeder

--- db/seeders/20221123042315-Users.js
+++ db/seeders/20221123042315-Users.js
@@ -3,44 +3,48 @@
 
 const bcrypt = require('bcryptjs');
 const admin = require('../../config/admin');
+const names = require('../names');
+const namesLen = names.length;
 
-const names = [
-  'Johnny',
-  'ucok',
-  'rambo,',
-];
+const users = [];
+const n = 50;
+const defaultImg = 'https://res.cloudinary.com/dgjwtquka/image/upload/c_scale,w_24/v1672668236/gosky/profile_h8kcr5.png';
+const date = new Date().toISOString();
+
+
+for (let i = 0; i < n; i++) {
+  const firstName = names[Math.floor(Math.random() * namesLen)];
+  const lastName = names[Math.floor(Math.random() * namesLen)];
+  users.push({
+    name: firstName + ' ' + lastName,
+    email: `${firstName}.${lastName}@gmail.com`,
+    role: 'USER',
+    imageId: 'default',
+    imageUrl: defaultImg,
+    phone: `0812343123${Math.floor((Math.random() * 99) + 1)}`,
+    address: 'Indonesia',
+    createdAt: date,
+    updatedAt: date,
+    encryptedPassword: bcrypt.hashSync('password'),
+  });
+};
+
+users.push({
+  name: admin.name,
+  email: admin.email,
+  encryptedPassword: bcrypt.hashSync(admin.password),
+  role: 'ADMIN',
+  imageId: 41131131,
+  imageUrl: defaultImg,
+  phone: '081234312343',
+  address: 'Indonesia',
+  createdAt: date,
+  updatedAt: date,
+});
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const password = 'user';
-    const timestamp = new Date();
-    const users = names.map((name) => ({
-      name,
-      email: `${name.toLowerCase()}@gmail.com`,
-      encryptedPassword: bcrypt.hashSync(password),
-      role: 'USER',
-      imageId: 12345678,
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1946/1946429.png',
-      phone: `0812343123${Math.floor((Math.random()*99) + 1)}`,
-      address: 'bandung',
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    }));
-
-    users.push({
-      name: admin.name,
-      email: admin.email,
-      encryptedPassword: bcrypt.hashSync(admin.password),
-      role: 'ADMIN',
-      imageId: 41131131,
-      imageUrl: 'https://www.shutterstock.com/image-vector/three-persons-admin-icon-outline-600w-1730974165.jpg',
-      phone: '081234312343',
-      address: 'jakarta',
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    });
-
     await queryInterface.bulkInsert('Users', users, {});
   },
 
